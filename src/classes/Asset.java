59da601f0b794dfaa68ac8b8b0bd1d34ef2395fe
Lucas Eskudlark <lucaseskudlark@gmail.com>
2021-12-29 10:04:51 -0300

    Configured Asset and Portfolio classes

--- src/classes/Asset.java
+++ src/classes/Asset.java
@@ -1,4 +1,60 @@
 package classes;
 
+import java.util.Date;
+
 public class Asset {
+    private String assetName;
+    private Double amount;
+    private Double pricePerUnit;
+    private Date purchaseDate;
+
+    // Default constructor
+    public Asset(){
+    }
+
+    // Constructor with arguments
+    public Asset(String assetName, Double amount, Double pricePerUnit, Date purchaseDate) {
+        this.assetName = assetName;
+        this.amount = amount;
+        this.pricePerUnit = pricePerUnit;
+        this.purchaseDate = purchaseDate;
+    }
+
+    // Getters and Setters
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(Double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Double getTotalInvested(){
+        return pricePerUnit * amount;
+    }
+
 }
--- src/classes/Portfolio.java
+++ src/classes/Portfolio.java
@@ -1,4 +1,81 @@
 package classes;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Portfolio {
+    private String ownerName;
+
+    private List<Asset> assetList = new ArrayList<>();
+
+    // Default constructor
+    public Portfolio(){
+    }
+
+    // Constructor with arguments
+    public Portfolio(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    // Getters and Setters
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public List<Asset> getAssetList() {
+        return assetList;
+    }
+
+    public void setAssetList(List<Asset> assetList) {
+        this.assetList = assetList;
+    }
+
+    // Method for adding assets to portfolio (Adding assets)
+    public void addAsset(Asset asset){
+        assetList.add(asset);
+    }
+
+    // Method for removing assets from portfolio (Removing asset or selling shares)
+    public void removeAsset(Asset asset){
+        assetList.remove(asset);
+    }
+
+    // Method to show invested assets (Asset name + price per unit + total invested + purchase date)
+    public void investedAssets(){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("---------- Invested Assets ----------");
+        // For each asset in the portfolio, print all asset info
+        for (Asset asset: assetList) {
+            System.out.println(
+                    " | Asset code/name: " + asset.getAssetName() +
+                    " | Price per Unit: U$" + asset.getPricePerUnit() +
+                    " | Total invested: U$" + String.format("%.2f", asset.getTotalInvested()) +
+                    " | Purchase date: " + sdf.format(asset.getPurchaseDate()));
+        }
+    }
+
+    // Method to show invested assets (investedAssets()) + total amount of shares + total amount invested
+    public void portfolioSummary(){
+        // Call investedAssets method
+        investedAssets();
+
+        /* For each asset in the portfolio, add the amount and amount invested in the variables totalAmount and
+        totalInvested; */
+        int totalAmount = 0;
+        double totalInvested = 0;
+        for (Asset asset : assetList) {
+            totalAmount += asset.getAmount();
+            totalInvested += asset.getTotalInvested();
+        }
+
+        System.out.println("---------- Summary ----------");
+        System.out.println("You have a total amount of " + totalAmount + " assets");
+        System.out.println("You have a total invested of U$" + totalInvested);
+    }
 }
