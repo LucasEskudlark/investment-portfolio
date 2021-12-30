cfe228637b9a2bd7ea6d39ce37d365b0c51d29e7
Lucas Eskudlark <lucaseskudlark@gmail.com>
2021-12-30 15:41:27 -0300

    Configured Program class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import classes.Asset;
+import classes.Portfolio;
+
+import java.io.*;
+import java.sql.SQLOutput;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
@@ -27,7 +31,7 @@ public class Program {
             // Option 1 - User wants to create a portfolio
             if (option == 1) {
                 // Request portfolio owner's name
-                System.out.println("Enter your name: ");
+                System.out.println("Enter the portfolio name: ");
                 String name = sc.nextLine();
 
                 // Create file path with the name entered
@@ -38,19 +42,117 @@ public class Program {
                                 "\\Portfolios\\" + name + ".txt";
 
                 // Create a .txt file where all info will be stored (Portfolio)
-                try (BufferedWriter bw = new BufferedWriter(new FileWriter(userPath))) {
-                    // Inform sucess
-                    System.out.println("Portfolio created successfully!");
-                } catch (IOException e) {
-                    System.out.println("Error creating portfolio: " + e.getMessage());
+                File f = new File(userPath);
+                // If there is already a .txt with the name entered, inform error. Else, create the file
+                if (f.exists()) {
+                    System.out.println("Error creating portfolio: Portfolio '" + name + "' already exists");
+                } else {
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+                        // Inform sucess
+                        System.out.println("Portfolio created successfully!");
+                    } catch (IOException e) {
+                        System.out.println("Error creating portfolio: " + e.getMessage());
+                    }
                 }
-
-
             }
 
 
             // Load portfolio option
+            if (option == 2) {
+                // Request the portfolio name
+                System.out.println("Enter the portfolio name: ");
+                String name = sc.nextLine();
 
+                // Create file path with the name entered and create a file with it
+                String portfolioPath =
+                        "C:\\Users\\" +
+                                System.getProperty("user.name") +
+                                "\\Desktop" +
+                                "\\Portfolios\\" + name + ".txt";
+                File f = new File(portfolioPath);
+
+                // Check if the portfolio .txt file exists
+                if (!f.exists()) {
+                    System.out.println("Error: Portfolio '" + name + "' does nos exists");
+                } else {
+                    // Inform success loading portfolio
+                    System.out.println("\nPortfolio '" + name + "' loaded successfully!");
+                    Portfolio portfolio = new Portfolio();
+
+                    try (BufferedReader br = new BufferedReader(new FileReader(f))){
+
+
+                        String itemStr = br.readLine();
+                        while (itemStr != null) {
+                            String[] fields = itemStr.split(",");
+
+                            String assetName = fields[0];
+                            double amount = Double.parseDouble(fields[1]);
+                            double pricePerUnit = Double.parseDouble(fields[2]);
+                            String purchaseDateStr = fields[3];
+
+                            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                            Date purchaseDate = sdf.parse(purchaseDateStr);
+
+                            Asset asset = new Asset(assetName, amount, pricePerUnit, purchaseDate);
+                            portfolio.addAsset(asset);
+
+                            itemStr = br.readLine();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    int operation = menu("Add asset", "Sell asset", "View invested assets", "Portfolio overview");
+
+                    // Add asset option
+                    if (operation == 1) {
+                        // Request asset name
+                        System.out.print("Enter asset name: ");
+                        String assetName = sc.nextLine();
+
+                        // Request asset amount
+                        System.out.print("Enter amount: ");
+                        int amount = sc.nextInt();
+
+                        // Request price per asset unit/share
+                        System.out.print("Enter price per unit: U$");
+                        double pricePerUnit = sc.nextDouble();
+
+                        // Add purchase date
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                        Date purchaseDate = new Date();
+                        String purchaseDateStr = sdf.format(purchaseDate);
+
+
+                        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f, true))){
+                                bw.newLine();
+                                bw.write(assetName + "," + amount + "," + String.format("%.2f", pricePerUnit) + "," + purchaseDateStr);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    // Sell asset option
+                    if (operation == 2) {
+                        System.out.print("Enter asset name: ");
+                        String sellAssetName = sc.nextLine();
+
+                        portfolio.sellAsset(sellAssetName);
+                    }
+
+                    // View invested assets option
+                    if (operation == 3) {
+                        portfolio.investedAssets();
+                    }
+
+                    // Portfolio overview
+                    if (operation == 4) {
+                        portfolio.Summary();
+                    }
+                }
+            }
 
             // Exit option
             if (option == 3) {
--- src/classes/Portfolio.java
+++ src/classes/Portfolio.java
@@ -3,6 +3,8 @@ package classes;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Portfolio {
     private String ownerName;
@@ -40,42 +42,84 @@ public class Portfolio {
         assetList.add(asset);
     }
 
-    // Method for removing assets from portfolio (Removing asset or selling shares)
+    // Method for removing assets from portfolio (Removing asset)
     public void removeAsset(Asset asset){
         assetList.remove(asset);
+    }
+
+    // Method to sell assets/shares
+    public void sellAsset(String assetName){
+        Scanner sc = new Scanner(System.in);
+        List<String> names = new ArrayList<>();
+
+        for (Asset asset : assetList) {
+            names.add(asset.getAssetName());
+        }
+
+        if (names.contains(assetName)) {
+            for (Asset asset : assetList) {
+                if (asset.getAssetName().equals(assetName)) {
+                    // Request the amount to be sold
+                    System.out.print("Enter the amount you want to sell: ");
+                    int amount = sc.nextInt();
+
+                    // Error if amount < 0 or amount > invested amount
+                    if (amount < 1 || amount > asset.getAmount()){
+                        System.out.println("Invalid amount");
+                    } else{
+                        // If only part of the total amount is going to be sold, set new amount
+                        asset.setAmount(asset.getAmount() - amount);
+                    }
+                }
+            }
+
+        } else {
+            System.out.println("\nAsset not found");
+        }
+
+
     }
 
     // Method to show invested assets (Asset name + price per unit + total invested + purchase date)
     public void investedAssets(){
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        System.out.println("---------- Invested Assets ----------");
-        // For each asset in the portfolio, print all asset info
-        for (Asset asset: assetList) {
-            System.out.println(
-                    " | Asset code/name: " + asset.getAssetName() +
-                    " | Price per Unit: U$" + asset.getPricePerUnit() +
-                    " | Total invested: U$" + String.format("%.2f", asset.getTotalInvested()) +
-                    " | Purchase date: " + sdf.format(asset.getPurchaseDate()));
+        if (assetList.size() != 0) {
+            System.out.println("---------- Invested Assets ----------");
+            // For each asset in the portfolio, print all asset info
+            for (Asset asset: assetList) {
+                System.out.println(
+                        "Asset code/name: " + asset.getAssetName() +
+                                " | Price per Unit: U$" + asset.getPricePerUnit() +
+                                " | Total invested: U$" + String.format("%.2f", asset.getTotalInvested()) +
+                                " | Purchase date: " + sdf.format(asset.getPurchaseDate()));
+            }
+        } else {
+            System.out.println("\nYou don't have any invested assets yet");
         }
     }
 
     // Method to show invested assets (investedAssets()) + total amount of shares + total amount invested
-    public void portfolioSummary(){
-        // Call investedAssets method
-        investedAssets();
+    public void Summary(){
+        if (assetList.size() != 0) {
+            // Call investedAssets method
+            investedAssets();
 
-        /* For each asset in the portfolio, add the amount and amount invested in the variables totalAmount and
-        totalInvested; */
-        int totalAmount = 0;
-        double totalInvested = 0;
-        for (Asset asset : assetList) {
-            totalAmount += asset.getAmount();
-            totalInvested += asset.getTotalInvested();
+            /* For each asset in the portfolio, add the amount and amount invested in the variables totalAmount and
+             totalInvested; */
+            int totalAmount = 0;
+            double totalInvested = 0;
+            for (Asset asset : assetList) {
+                totalAmount += asset.getAmount();
+                totalInvested += asset.getTotalInvested();
+            }
+
+            System.out.println("---------- Summary ----------");
+            System.out.println("You have a total amount of " + totalAmount + " shares");
+            System.out.println("You have a total invested of U$" + totalInvested);
+
+        } else {
+            System.out.println("\nThe portfolio is empty.");
         }
 
-        System.out.println("---------- Summary ----------");
-        System.out.println("You have a total amount of " + totalAmount + " assets");
-        System.out.println("You have a total invested of U$" + totalInvested);
     }
 }
